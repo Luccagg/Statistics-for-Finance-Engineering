30fd2bd18b754c322e4a9e15f57386e45807a998
Lucca Gontijo Assis <38539362+Luccagg@users.noreply.github.com>
2026-09-04 01:36:46 -0300

    Fix assignment operator and improve readability

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
-dat = read.csv('datasets/Stock_Bond.csv')
+dat <- read.csv('datasets/Stock_Bond.csv')
 attach(dat)
-n = dim(dat)[1]
-GMReturn = GM_AC[-1]/GM_AC[-n] - 1
-FReturn = F_AC[-1]/F_AC[-n] - 1
-par(mfrow = c(1,1))
+n <- dim(dat)[1]
+GMReturn <- GM_AC[-1]/GM_AC[-n] - 1
+FReturn <- F_AC[-1]/F_AC[-n] - 1
+par(mfrow <- c(1,1))
 plot(GMReturn, FReturn)
