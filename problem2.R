9d5b29190171dd2cb80bd22e835baea0007a5de6
Lucca Gontijo Assis <lgontijoassis@gmail.com>
2026-09-22 01:59:39 -0300

    Update

--- problem2.R
+++ problem2.R
@@ -5,5 +5,6 @@ n <- dim(dat)[1]
 # on the adjusted closing price (represented by the GM_AC column)
 GMReturn <- GM_AC[-1] / GM_AC[-n] - 1 # simple return
 LogGMReturn <- log(GM_AC[-1] / GM_AC[-n]) # log return
+CorReturns <- cor(GMReturn, LogGMReturn)
 par(mfrow = c(1, 1))
 plot(GMReturn, LogGMReturn)
