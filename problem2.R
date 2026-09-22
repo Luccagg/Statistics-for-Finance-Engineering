a465e060702c262cb5094195114f11aa33fce2fe
Lucca Gontijo Assis <lgontijoassis@gmail.com>
2026-09-22 01:03:36 -0300

    update prob2

---
+++ problem2.R
@@ -0,0 +1,9 @@
+dat <- read.csv("datasets/Stock_Bond.csv")
+attach(dat)
+n <- dim(dat)[1]
+# Computes the comulative simple return of General Motors bc is based
+# on the adjusted closing price (represented by the GM_AC column)
+GMReturn <- GM_AC[-1] / GM_AC[-n] - 1 # simple return
+LogGMReturn <- log(GM_AC[-1] / GM_AC[-n]) # log return
+par(mfrow = c(1, 1))
+plot(GMReturn, LogGMReturn)
